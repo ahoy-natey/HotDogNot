13f6d03798c7b89ccf73e96242a6a67a9592e952
NathanStein1 <nathanstein2@gmail.com>
2022-10-27 15:04:53 -0700

    import tensorflowastf

--- Code/main.py
+++ Code/main.py
@@ -1,4 +1,18 @@
 import tensorflow as tf
+import matplotlib as pit
+import os
 import numpy as np
-import pandas as py
+from tensorflow import keras
+from keras.preprocessing.image import ImageDataGenerator, array_to_img, img_to_array
 
+
+datagen = ImageDataGenerator(
+    rotation_range=40,
+    width_shift_range=.02,
+    height_shift_range=0.2,
+    rescale=1./255,
+    shear_range=0.2,
+    zoom_range=0.2,
+    horizontal_flip=True,
+    fill_mode='nearest'
+)
